b2325087b417ad11020843c1fe2f4657ed6a1e31
Jed Grant <jed@kyc3.com>
2026-02-26 13:03:10 +0100

    Generalize test runner to use any sample files and log full results

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -16,12 +16,9 @@ echo "[INFO] Copying source samples..."
 cp -R "${SRC}" "${WORK}/source"
 
 echo "[INFO] Seeding dupes folder..."
-cp "${WORK}/source/Hard Link Hearts.mp3" "${WORK}/dupes/"
-cp "${WORK}/source/Hard Link Hearts (1).mp3" "${WORK}/dupes/"
-cp "${WORK}/source/One Beating Core.mp3" "${WORK}/dupes/"
-cp "${WORK}/source/One Beating Core (1).mp3" "${WORK}/dupes/"
-cp "${WORK}/source/0bytes.mp3" "${WORK}/dupes/"
-cp "${WORK}/source/BadAudio.mp3" "${WORK}/dupes/"
+find "${WORK}/source" -maxdepth 1 -type f -name "*.mp3" | head -n 6 | while read -r f; do
+  cp "$f" "${WORK}/dupes/"
+done
 
 run_cmd() {
     local label="$1"
@@ -32,10 +29,17 @@ run_cmd() {
 }
 
 echo "[INFO] Running scans and duplicate checks..."
-run_cmd "scan (file+audio)" ./fhash scan -r -h -a -f -s "${WORK}" -e mp3 -d "${DB}"
-run_cmd "dupe file hash" ./fhash dupe -xh2 -s "${WORK}" -r -e mp3 -d "${DB}"
-run_cmd "dupe audio hash" ./fhash dupe -xa2 -s "${WORK}" -r -e mp3 -d "${DB}"
-run_cmd "link dry-run (file hash, shallowest)" ./fhash link -xh2 -ls -s "${WORK}/dupes" -r -e mp3 -d "${DB}" -dry
-run_cmd "db contents" sqlite3 "${DB}" "SELECT filepath, md5, audio_md5 FROM files ORDER BY filepath;"
+run_cmd "scan (file+audio)" ./fhash scan -v -r -h -a -f -s "${WORK}" -e mp3 -d "${DB}"
+run_cmd "post-scan db" sqlite3 "${DB}" "SELECT filepath, md5, audio_md5 FROM files ORDER BY filepath;"
+run_cmd "post-scan ll" ls -lR "${WORK}"
+
+run_cmd "dupe file hash" ./fhash dupe -v -xh2 -s "${WORK}" -r -e mp3 -d "${DB}"
+run_cmd "post-dupe db" sqlite3 "${DB}" "SELECT filepath, md5, audio_md5 FROM files ORDER BY filepath;"
+
+run_cmd "dupe audio hash" ./fhash dupe -v -xa2 -s "${WORK}" -r -e mp3 -d "${DB}"
+run_cmd "post-dupe-audio db" sqlite3 "${DB}" "SELECT filepath, md5, audio_md5 FROM files ORDER BY filepath;"
+
+run_cmd "link dry-run (file hash, shallowest)" ./fhash link -v -xh2 -ls -s "${WORK}/dupes" -r -e mp3 -d "${DB}" -dry
+run_cmd "post-link dry-run ll" ls -lR "${WORK}"
 
 echo "[INFO] Results written to ${OUT}"
